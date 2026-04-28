f9d5175518947b12d21d36e81fd756640d7e1445
Oumarvi <omar_vi122076@outlook.com>
2026-04-28 10:58:09 -0500

    Update settings.py (Project)

--- proj2/proj2/settings.py
+++ proj2/proj2/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,3 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / STATIC_URL,
+]
+
+STATIC_ROOT = BASE_DIR / "assets"
