4a0678a2c9e906999892c2cac8e135856fa29436
fizagui2 <izaguirrefrank6190@yahoo.com>
2026-05-06 15:22:55 -0500

    Added functional signup, login, and newsletter systems

--- proj2/app2/views.py
+++ proj2/app2/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 from .models import (
     Slider,
@@ -11,6 +13,7 @@ from .models import (
     TrailerItem,
     News,
     Tweet,
+    NewsletterSubscriber,
 )
 
 
@@ -39,3 +42,52 @@ def home(request):
 
 def moviesingle(request):
     return render(request, 'app2/moviesingle.html')
+
+
+def newsletter(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+
+        if email:
+            NewsletterSubscriber.objects.get_or_create(email=email)
+
+    return redirect('home')
+
+
+def signup(request):
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if username and email and password:
+
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+
+    return redirect('home')
+
+
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+
+    return redirect('home')
